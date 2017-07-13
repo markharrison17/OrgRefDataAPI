6d391339660ce9c4775ae71d0ca879257b0b4f2c
markharrison <mark.harrison17@nhs.net>
2017-07-13 13:34:35 +0100

    adding files to repo

---
+++ main.py
@@ -0,0 +1,18 @@
+""" This module is the root of the application and contains the main startup
+class."""
+import logging
+
+def do_setup():
+    """Setup for the application logger."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    #handler = DefaultStreamHandler()
+    #logger.addHandler(handler)
+    logger.info('configured logger')
+
+
+    logger.info("starting agent listener")
+
+
+if __name__ == "__main__":
+    do_setup()

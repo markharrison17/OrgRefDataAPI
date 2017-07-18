05cd6ca0e7584c0498871d35c376b8a3404a5057
markharrison <mark.harrison17@nhs.net>
2017-07-18 09:45:57 +0100

    adding files to repo

--- unit_tests/test_main.py
+++ unit_tests/test_main.py
@@ -1,18 +1,28 @@
-""" Unit tests for the main entry point for the open ods api service. """
 import unittest
 from mock import patch, MagicMock
 import main
 import logging
 
 
-class TestMain(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
 
-    @patch('main.DefaultStreamHandler')
-    @patch('main.ServiceManager')
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
     @patch('logging.getLogger')
-    def test__main__do_setup__WillInstantiateCustomStreamHandler_WhenCalled(self,
-                                                                        mock_default_stream_handler,
-                                                                        mock_service_manager,
-                                                                        mock_get_logger):
+    def test__main__do_setup__TestNonsense(self, mock_get_logger):
         main.do_setup()
-        mock_default_stream_handler.assert_called_with()
+
+if __name__ == '__main__':
+    unittest.main()
